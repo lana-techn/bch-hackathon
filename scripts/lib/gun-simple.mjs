a1e911cb626ad4a3fd0682df0d7d00391bf2f758
maulana-tech <firdaussyah03@gmail.com>
2026-02-11 20:44:42 +0700

    chore: Add utility scripts for Web3
    - save-token-web3.mjs: Save token to Gun.js database
    - test-ipfs.mjs: Test IPFS upload
    - lib/gun-simple.mjs: Simplified Gun.js module for scripts

---
+++ scripts/lib/gun-simple.mjs
@@ -0,0 +1,33 @@
+/**
+ * Simple Gun.js database module for scripts (CommonJS compatible)
+ */
+
+import Gun from 'gun';
+
+const gun = Gun({
+  peers: [
+    'https://gun-manhattan.herokuapp.com/gun',
+    'https://gun-us.herokuapp.com/gun',
+  ],
+  localStorage: false,
+  radisk: true,
+});
+
+const tokensNode = gun.get('ignitebch_tokens');
+
+export async function saveTokenMetadata(token) {
+  return new Promise((resolve, reject) => {
+    tokensNode.get(token.id).put(token, (ack) => {
+      if (ack?.err) reject(new Error(ack.err));
+      else resolve();
+    });
+  });
+}
+
+export async function getTokenMetadata(tokenId) {
+  return new Promise((resolve) => {
+    tokensNode.get(tokenId).once((data) => {
+      resolve(data);
+    });
+  });
+}
